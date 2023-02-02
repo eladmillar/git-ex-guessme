b6ddb0aa9a30b85a0e2dafe5be829d49f6d7de0c
Elad Millar <eladmillar@gmail.com>
2023-02-02 18:46:54 +0200

    working

--- js/quest.controller.js
+++ js/quest.controller.js
@@ -15,32 +15,39 @@ function init() {
 }
 
 function onStartGuessing() {
-  // TODO: hide the game-start section
-
+  // TODO: hide the game-start section - DONE
+  $('.game-start').hide()
   renderQuest()
-  // TODO: show the quest section
+  // TODO: show the quest section - DONE
+  $('.quest').show()
 }
 
 function renderQuest() {
-  // TODO: select the <h2> inside quest and update
-  // its text by the currQuest text
+  // TODO: select the <h2> inside quest and update - DONE
+  // its text by the currQuest text - DONE
+  $('.quest h2').text(getQuestText())
 }
 
 function onUserResponse(ev) {
-  console.log('ev', ev)
-  var res = ev.data.ans
+  // console.log('ev', ev)
+  const res = ev.data.ans
   // If this node has no children
   if (isChildless(getCurrQuest())) {
     if (res === 'yes') {
-      alert('Yes, I knew it!')
+      $('.quest h2').text('Yes, I knew it!')
+      // alert('Yes, I knew it!')
       // TODO: improve UX
     } else {
+      // $('.quest h2').text('I dont know...teach me!')
       alert('I dont know...teach me!')
-      // TODO: hide and show new-quest section
+      // TODO: hide and show new-quest section - DONE
+      $('.quest').hide()
+      $('.new-quest').show()
     }
   } else {
-    // TODO: update the lastRes global var
-    moveToNextQuest(res)
+    // TODO: update the lastRes global var - DONE
+    gLastRes = res
+    moveToNextQuest(gLastRes)
     renderQuest()
   }
 }
@@ -49,16 +56,16 @@ function onAddGuess(ev) {
   ev.preventDefault()
   var newGuess = $('#newGuess').val()
   var newQuest = $('#newQuest').val()
-
-  // TODO: Get the inputs' values
-  // TODO: Call the service addGuess
-
+  // TODO: Get the inputs' values -DONE
+  // TODO: Call the service addGuess -DONE
+  addGuess(newQuest, newGuess, gLastRes)
   onRestartGame()
 }
 
 function onRestartGame() {
   $('.new-quest').hide()
   $('.game-start').show()
+  createQuestsTree()
   gLastRes = null
 }
 
--- js/quest.service.js
+++ js/quest.service.js
@@ -3,13 +3,19 @@
 var gQuestsTree
 var gCurrQuest
 var gPrevQuest = null
+const STORAGE_KEY = 'questDB'
 
 function createQuestsTree() {
-  gQuestsTree = createQuest('Male?')
-  gQuestsTree.yes = createQuest('Gandhi')
-  gQuestsTree.no = createQuest('Rita')
+  gQuestsTree = loadFromStorage(STORAGE_KEY, gQuestsTree)
+  // console.log('DB' ,loadFromStorage(STORAGE_KEY, gQuestsTree));
+  if (!gQuestsTree) {
+    gQuestsTree = createQuest('Male?')
+    gQuestsTree.yes = createQuest('Gandhi')
+    gQuestsTree.no = createQuest('Rita')
+  }
   gCurrQuest = gQuestsTree
   gPrevQuest = null
+  console.log('gQuestsTree', gQuestsTree)
 }
 
 function createQuest(txt) {
@@ -20,16 +26,29 @@ function createQuest(txt) {
   }
 }
 
+function getQuestText() {
+  return gCurrQuest.txt
+}
+
 function isChildless(node) {
   return node.yes === null && node.no === null
 }
 
 function moveToNextQuest(res) {
-  // TODO: update the gPrevQuest, gCurrQuest global vars
+  // TODO: update the gPrevQuest, gCurrQuest global vars - DONE
+  gPrevQuest = gCurrQuest
+  gCurrQuest = gCurrQuest[res]
+  console.log('gCurrQuest', gCurrQuest)
 }
 
 function addGuess(newQuestTxt, newGuessTxt, lastRes) {
-  // TODO: Create and Connect the 2 Quests to the quetsions tree
+  // TODO: Create and Connect the 2 Quests to the questions tree - DONE
+  var newQuest = createQuest(newQuestTxt)
+  var newGuess = createQuest(newGuessTxt)
+  newQuest.yes = newGuess
+  gPrevQuest[lastRes] = newQuest
+  newQuest.no = gCurrQuest
+  saveToStorage(STORAGE_KEY, gQuestsTree)
 }
 
 function getCurrQuest() {
